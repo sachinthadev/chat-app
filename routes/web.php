120a3e9f82c5da1b4c2da674d8843f9b5d4a5da8
SahanSachintha <131697812+SahanSachintha@users.noreply.github.com>
2024-02-19 22:52:26 +0530

    Class "Redis" not found

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatController;
+use Illuminate\Support\Facades\Redis;
 
 /*
 |--------------------------------------------------------------------------
