f142edeecf253440672d6bb51ee2023fd9fda42e
SahanSachintha <131697812+SahanSachintha@users.noreply.github.com>
2024-02-19 20:46:53 +0530

    create chat controller and Update the ChatController to send and receive messages:

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -10,27 +10,32 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent
+class MessageSent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+    use SerializesModels;
 
     /**
      * Create a new event instance.
      */
-    public function __construct()
-    {
-        //
-    }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
-    }
+     public $message;
+     public $recipient;
+ 
+     public function __construct($message, $recipient)
+     {
+         $this->message = $message;
+         $this->recipient = $recipient;
+     }
+ 
+     public function broadcastOn()
+     {
+         return new PrivateChannel('chat.' . $this->recipient);
+     }
+
+
+
+   
 }
+
+
--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Events\MessageSent;
 
 class ChatController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('chat');
+    }
+
+    public function sendMessage(Request $request)
+    {
+        $message = $request->input('message');
+        $recipient = $request->input('recipient');
+        event(new MessageSent($message, $recipient));
+        return response()->json(['message' => 'Message sent!']);
+    }
 }
