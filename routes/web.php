75b7d8361c8fe6fd728ad770b52b9da9d9b5f693
SahanSachintha <131697812+SahanSachintha@users.noreply.github.com>
2024-02-19 20:25:33 +0530

    create chat.blade.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/chat', [ChatController::class, 'index']);
+Route::post('/send-message', [ChatController::class, 'sendMessage']);
