523cc91a6ed8cb744a1010e603aaff34e098c544
SahanSachintha <131697812+SahanSachintha@users.noreply.github.com>
2024-02-26 22:39:10 +0530

    redis not found error use Illuminate\Support\Facades\Redis path not found

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -9,16 +9,13 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Redis;
+
 
 class MessageSent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     use SerializesModels;
 
-    /**
-     * Create a new event instance.
-     */
 
      public $message;
      public $recipient;
--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Events\MessageSent;
-use Illuminate\Support\Facades\Redis;
 
 class ChatController extends Controller
 {
